d1244d1ed1da079069879ac272e8a2b4a993412c
Animesh Bhakat <bhakat.animesh1024@gmail.com>
2024-02-20 10:09:05 +0530

    change spped to dynamic using + and - button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var intervalId;
 var speed = 100; // Default scrolling speed (milliseconds)
+var showSpeed = speed;
 var isScrolling = false;
 
 function autoScroll() {
@@ -18,10 +19,30 @@ function toggleScrolling() {
     isScrolling = !isScrolling;
 }
 
-function changeSpeed(newSpeed) {
-    speed = newSpeed; // Update the speed variable
-    if (isScrolling) {
-        clearInterval(intervalId); // Stop auto-scrolling if it's currently scrolling
-        intervalId = setInterval(autoScroll, speed); // Restart auto-scrolling with the new speed
-    }
+// function changeSpeed(newSpeed) {
+//     speed = newSpeed; // Update the speed variable
+//     if (isScrolling) {
+//         clearInterval(intervalId); // Stop auto-scrolling if it's currently scrolling
+//         intervalId = setInterval(autoScroll, speed); // Restart auto-scrolling with the new speed
+//     }
+// }
+
+function increaseSpeed(){
+  speed = speed - 10;
+  showSpeed = showSpeed + 10;
+  document.getElementById('speedCounter').textContent = showSpeed;
+  if(isScrolling){
+    clearInterval(intervalId);
+    intervalId = setInterval(autoScroll, speed);
+  }
+}
+
+function decreaseSpeed(){
+  speed = speed + 10;
+  showSpeed = showSpeed - 10;
+  document.getElementById('speedCounter').textContent = showSpeed;
+  if(isScrolling){
+    clearInterval(intervalId);
+    intervalId = setInterval(autoScroll, speed);
+  }
 }
